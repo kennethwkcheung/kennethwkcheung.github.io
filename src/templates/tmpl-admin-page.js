743f70aab832138c366157b6d14d8fa879c05bbd
Kenneth Cheung <kennethwkcheung@ird.gov.hk>
2020-11-03 10:28:54 +0800

    Prepare input form for adding new blog post

--- src/templates/tmpl-admin-page.js
+++ src/templates/tmpl-admin-page.js
@@ -1,13 +1,15 @@
 import React from "react"
 
 import Template from '../components/_layout/_template/template'
-import Form from '../components/_data/_form/form'
+import Form from '../components/_data/_form/form'            
 
 export default ({pageContext}) => {
     return (
         <Template>
             <div className="row">
-                ADMIN PAGE
+                <div className={`col nopadding`}>
+                    ADMIN PAGE
+                </div>
             </div>
             <Form />
         </Template >
