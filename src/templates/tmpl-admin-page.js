870a78d0c80b29d53895519831d3946f8ac39dd6
Kenneth Cheung <kennethwkcheung@ird.gov.hk>
2020-11-18 17:44:15 +0800

    Added wish list in admin page

--- src/templates/tmpl-admin-page.js
+++ src/templates/tmpl-admin-page.js
@@ -4,26 +4,37 @@ import Template from '../components/_layout/_template/template'
 import Form from '../components/_data/_form/form'            
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpaceShuttle, faGlobeAsia } from '@fortawesome/free-solid-svg-icons'
+import { faSpaceShuttle, faGlobeAsia, faDatabase, faFile, faArchive, faSearch, faTags, faBox } from '@fortawesome/free-solid-svg-icons'
 
 export default ({pageContext}) => {
-    const elastic='http://127.0.0.1:9200'
-    
+
     return (
         <Template>
-            <div className='row'>
-                <div className={'col col-sm-4'}>
-                    LEFT
+            <div className='row pt-2 pb-2'>
+                <div className={'col col-sm-1 d-flex justify-content-center'}>
+                    <FontAwesomeIcon icon={ faDatabase } />&nbsp;FLOOD
                 </div>
-                <div className={'col col-sm-4'}>
+                <div className={'col col-sm-1 d-flex justify-content-center'}>
+                    <FontAwesomeIcon icon={ faFile } />&nbsp;FILE
+                </div>
+                <div className={'col col-sm-1 d-flex justify-content-center'}>
+                    <FontAwesomeIcon icon={ faArchive } />&nbsp;PACK
+                </div>
+                <div className={'col col-sm-6 d-flex justify-content-center'}>
                     <span className="fa-layers fa-fw">
-                        <FontAwesomeIcon icon={faGlobeAsia} spin color="#CCCCCC" />
-                        <FontAwesomeIcon icon={ faSpaceShuttle } transform={{ rotate:315 }} color="#666666" />
+                        <FontAwesomeIcon icon={ faGlobeAsia } spin color="#AAAAAA" />
+                        <FontAwesomeIcon icon={ faSpaceShuttle } transform={{ rotate:315 }} color="#000000" />
                     </span>
                     ADMIN PAGE
                 </div>
-                <div className={'col col-sm-4'}>
-                    RIGHT
+                <div className={'col col-sm-1 d-flex justify-content-center'}>
+                    <FontAwesomeIcon icon={ faTags } />&nbsp;TAGS
+                </div>
+                <div className={'col col-sm-1 d-flex justify-content-center'}>
+                    <FontAwesomeIcon icon={ faSearch } />&nbsp;SEARCH
+                </div>
+                <div className={'col col-sm-1 d-flex justify-content-center'}>
+                    <FontAwesomeIcon icon={ faBox } />&nbsp;BULK
                 </div>
             </div>
             <Form />
